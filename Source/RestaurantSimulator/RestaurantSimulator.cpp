097775195ed3c66fe9771d6223563b67e0ba3f0f
DESKTOP-AFTO5BH\tigloos-intern <etsnlp@gmail.com>
2023-06-21 13:47:22 +0300

    Project Init

---
+++ Source/RestaurantSimulator/RestaurantSimulator.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RestaurantSimulator.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RestaurantSimulator, "RestaurantSimulator" );
